4c31f0e121611540d733cc09dce009eb9ebe2459
Gregor von Laszewski <laszewski@gmail.com>
2019-04-16 14:16:21 -0400

    fixing the README.yml

---
+++ bin/readme.py
@@ -0,0 +1,44 @@
+from  cloudmesh_installer.install.installer import repos
+from pprint import pprint
+import oyaml as yaml
+from cloudmesh.common.util import readfile
+#pprint(repos)
+from cloudmesh.DEBUG import VERBOSE
+from tabulate import tabulate
+from cloudmesh.common.dotdict import dotdict
+
+repos_516 = []
+repos_222 = []
+
+for repo in repos["spring19"]:
+    if "-222-" in repo:
+        repos_222.append(repo)
+    else:
+        repos_516.append(repo)
+
+print (repos_222)
+print (repos_516)
+
+
+community="https://github.com/cloudmesh-community"
+
+def class_table(repos):
+    global community
+    headers = ["hid", "firstname", "lastname", "community", "semester", "url"]
+    t = []
+
+    for repo in repos:
+        print()
+        VERBOSE(repo)
+        content = readfile(f"./../{repo}/README.yml")
+        data = yaml.load(content, Loader=yaml.SafeLoader)
+        owner = dotdict(data["owner"])
+        url = f"{community}/{repo}"
+        t.append([owner.hid, owner.firstname, owner.lastname, owner.community, owner.semester, url])
+
+    print(tabulate(t, headers=headers))
+
+
+class_table(repos_222)
+
+class_table(repos_516)
